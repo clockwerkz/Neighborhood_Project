7048107f267049ba4cd940dddc65142660508ac3
Carlos <clockwerkz@gmail.com>
2018-09-22 00:16:34 -0700

    map works now thanks to Stack Overflow entry

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GoogleMap from './GoogleMap';
 
 class App extends Component {
   
-  componentDidMount() {
-    window.initMap = this.initMap;
-    loadJS('https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap');
-  }
-
-  initMap() {
-    map = new google.maps.Map(ReactDOM.findDOMNode(this.refs.map));
-  }
   
   render() {
     return (
       <div className="App">
-        <div ref="map" style={{height: '100vh', width: '100%', background: '#444'}}></div>
+        <GoogleMap />
       </div>
     );
   }
-}
-
-function loadJS(src) {
-  let ref = window.document
 }
 
 export default App;
---
+++ src/GoogleMap.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+
+class GoogleMap extends Component {
+    state = {
+        mapIsReady : false
+    }
+
+    componentDidMount() {
+        const ApiKey = 'AIzaSyAEPrt7HQliWl1YkdstUsF7rBqs94J2axc';
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}&v=13`;
+        script.async = true;
+        script.defer = true;
+        script.addEventListener('load', ()=> {
+            this.setState({mapIsReady : true});
+        })
+
+        document.body.appendChild(script);
+    }
+
+    componentDidUpdate() {
+        if (this.state.mapIsReady) {
+            this.map = new window.google.maps.Map(document.getElementById('map'), {
+                center: {lat: 25.7617, lng: -80.1918},
+                zoom: 12, 
+            });
+        }
+    }
+
+
+    render() {
+        return (
+            <div id="map"></div>
+        )
+    }
+}
+
+export default GoogleMap
